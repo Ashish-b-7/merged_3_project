82aa181be365d6b95fc3cd41b4759e4381df4be4
Ashish Bhardwaj <124138178+Ashish-b-7@users.noreply.github.com>
2023-11-09 15:02:39 +0530

    Misceleneous_excel.py

--- Misceleneous_excel.py
+++ Misceleneous_excel.py
@@ -2,42 +2,52 @@ import pandas as pd
 from flask import Flask, render_template, request, send_file
 from flask import Blueprint, render_template
 import os
+
 misc_app = Blueprint("misc_app", __name__)
 
 if not os.path.exists('uploads'):
     os.makedirs('uploads')
 
+UPLOAD_FOLDER = 'uploads'
+
 @misc_app.route('/')
 def index():
     return render_template('Misc_index.html')
 
-@misc_app.route('/process_data', methods=['POST'])
-def process_data():
-    try:
-        file = request.files['file']
-        if file:
-            df = pd.read_excel(file)
+@misc_app.route('/process_excel', methods=['POST'])
+def process_excel():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return "No file part"
+    
+    file = request.files['file']
 
-            agg_results = df.groupby(['Project', 'RV', 'Mapping Dates'])[' Hours'].agg(['sum']).reset_index()
+    if file.filename == '':
+        return "No selected file"
+    
+    if file and file.filename.endswith('.xlsx'):
+        # Save the uploaded file
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+        
+        input_file_path = os.path.join(UPLOAD_FOLDER, file.filename)
 
-            agg_results.columns = ['Project', 'RV', 'Mapping Dates', 'Total Hours']
+        # Use 'Hours' if present, otherwise use ' Hours'
+        column_name = 'Hours' if 'Hours' in pd.read_excel(input_file_path, nrows=1).columns else ' Hours'
+        df = pd.read_excel(input_file_path, usecols=['Project', 'RV', 'Mapping Dates', column_name])
 
-            agg_results = agg_results['Project', 'Mapping Dates', 'RV', 'Total Hours']
+        res = df.groupby(['Project', 'RV', 'Mapping Dates'])[column_name].sum().reset_index()
 
-            transformed_data = agg_results.pivot(index='Project', columns=['Mapping Dates', 'RV'], values='Total Hours').reset_index()
+        columns_order = ['Project', 'Mapping Dates', 'RV', column_name]
 
-            writer = pd.ExcelWriter('transformed_results.xlsx')
-            transformed_data.to_excel(writer, sheet_name='Transformed Results', index=True)
-            writer.save()
+        transformed_data = res.pivot_table(index='Project', columns=['Mapping Dates', 'RV'], values=column_name, fill_value=0).reset_index()
 
-            return 'Data processed and saved to transformed_results.xlsx'
-        else:
-            return 'No file provided.'
-    except Exception as e:
-        return f'An error occurred: {str(e)}'
+        output_file_name = f"output_{file.filename}"
+        output_file_path = os.path.join(UPLOAD_FOLDER, output_file_name)
 
-@misc_app.route('/download')
-def download():
-    return send_file('transformed_results.xlsx', as_attachment=True)
-
-
+        # Save the result to the output Excel file
+        with pd.ExcelWriter(output_file_path) as writer:
+            transformed_data.to_excel(writer, sheet_name="Transform_Results", index=True)
+        
+        return send_file(output_file_path, as_attachment=True)
+    else:
+        return "Invalid file format. Please upload an Excel file."
